dccddafaba755e0728639ef73a05b0e453f4cd2a
nxn <15559628673@163.com>
2018-05-08 20:21:01 +0800

    初建登录

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1>{{msg}}</h1>
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: '这才是登录页'
+    }
+  }
+}
+</script>
+<style>
+
+</style>
